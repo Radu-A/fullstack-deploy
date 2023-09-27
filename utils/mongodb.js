1d81de21ac8360d6f80ee537bd93174bf8aabdf6
Radu-A <elcastravete@gmail.com>
2023-09-27 11:00:21 +0200

    added/mongoose_config

--- utils/mongodb.js
+++ utils/mongodb.js
@@ -1,3 +1,10 @@
 const mongoose = require('mongoose');
+require('dotenv').config;
 
-mongoose.connect(``)
+mongoose
+    .connect(`mongodb+srv://${process.env.ATLAS_USER}:${process.env.ATLAS_PASSWORD}@cluster0.lhlyvrl.mongodb.net/`)
+    .catch(error=>handleError(error)) //Manejo de error recomendado por la documentación
+
+const db = mongoose.connection;
+
+// 
